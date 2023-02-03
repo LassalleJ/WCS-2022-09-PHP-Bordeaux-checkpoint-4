1cc89bfe0ef3a38498f527dc29484341aee7c843
Jordan Lassalle <lassalle.jordan@gmail.com>
2023-02-03 09:23:03 +0100

    start the group creation

--- src/Form/GroupType.php
+++ src/Form/GroupType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Group;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +13,11 @@ class GroupType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('isFull')
-            ->add('createdAt')
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control text-center'
+                ],
+            ])
         ;
     }
 
